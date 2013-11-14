afdacf23b484e40728049990562160889d88177c
heapdump <gordon.fischer@gmail.com>
2013-11-13 23:08:41 -0600

    Create RoundRockPublicLibrary_min.js

---
+++ RoundRockPublicLibrary_min.js
@@ -0,0 +1,1 @@
+javascript:var r=/([\/-]|is[bs]n=)(\d{7,9}[\dX])/i;if(r.test(location.href)==true){var isbn=RegExp.$2;window.open("http://cat.round-rock.tx.us:8080/ipac20/ipac.jsp?index=ISBNEX&term="+isbn)}
