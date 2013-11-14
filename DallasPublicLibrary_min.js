7aa437be51d749920a2aa6316e1e867e39fc47f5
heapdump <gordon.fischer@gmail.com>
2013-11-13 23:04:24 -0600

    Create DallasPublicLibrary_min.js

---
+++ DallasPublicLibrary_min.js
@@ -0,0 +1,1 @@
+javascript:var r=/([\/-]|is[bs]n=)(\d{7,9}[\dX])/i;if(r.test(location.href)==true){var isbn=RegExp.$2;window.open("http://catalog.dallaslibrary.org/polaris/search/searchresults.aspx?by=ISBN&term="+isbn)}
